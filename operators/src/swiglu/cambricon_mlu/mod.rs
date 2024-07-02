26dcc15f965c8d49e91bd47806e75227a57308fb
YdrMaster <ydrml@hotmail.com>
2024-07-02 12:22:55 +0800

    添加mlu swiglu算子

--- operators/src/swiglu/cambricon_mlu/mod.rs
+++ operators/src/swiglu/cambricon_mlu/mod.rs
@@ -99,7 +99,8 @@ impl common::Operator for Operator {
 
         let dt = DataType::from(dt);
         let gate = Tensor::new(dt, &[n as _, d as _], &[sgn as _, sgd as _]);
-        let up = Tensor::new(dt, &[], &[]);
+        let up = Tensor::new(dt, &[n as _, d as _], &[sun as _, sud as _]);
+        let input = Tensor::new(dt, &[n as _, (2 * d) as i64], &[(2 * sgn) as i64, sgd as _]);
 
         let mut workspace_size = 0;
 
@@ -109,7 +110,32 @@ impl common::Operator for Operator {
                 2,
                 &mut workspace_size
             ));
-            queue.ctx().malloc::<u8>(workspace_size);
+            let mut workspace = queue.ctx().malloc::<u8>(workspace_size);
+            let input_size = 2 * d * ((unit) as usize) * n;
+            let mut input_base = queue.ctx().malloc::<u8>(input_size);
+            cnnl!(cnnlConcat(
+                handle.as_raw(),
+                2,
+                -1,
+                [gate.as_raw(), up.as_raw()].as_ptr(),
+                [gate_base.cast::<u8>(), up_base.cast::<u8>()]
+                    .as_ptr()
+                    .cast(),
+                workspace.as_mut_ptr().cast(),
+                workspace_size,
+                input.as_raw(),
+                input_base.as_mut_ptr().cast(),
+            ));
+            cnnl!(cnnlBiasActivationGluForward_v2(
+                handle.as_raw(),
+                self.glu,
+                input.as_raw(),
+                input_base.as_mut_ptr().cast(),
+                null_mut(),
+                null_mut(),
+                gate.as_raw(),
+                gate_base.cast(),
+            ));
         });
 
         Ok(())
