58a8e11bf7cbaa2bf9d9b52a2175be92e5a3cf7f
YdrMaster <ydrml@hotmail.com>
2024-09-12 11:14:05 +0800

    test: 将 rope nv 与 cpu/f64 对比精度
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- operators/src/rope/nvidia_gpu/mod.rs
+++ operators/src/rope/nvidia_gpu/mod.rs
@@ -130,38 +130,159 @@ extern "C" __global__ void {NAME}(
     }
 }
 
-#[test]
-fn test() {
-    use common::{dyn_, Operator as _, TensorLayout};
-    use digit_layout::types::{F32, U32};
-    use std::ptr::{null, null_mut};
+#[cfg(test)]
+mod test {
+    use super::{Args, Gpu, Operator};
+    use common::{Handle, Operator as _, TensorLayout};
+    use digit_layout::{
+        types::{F16, F64, U32},
+        DigitLayout,
+    };
 
-    if let Err(cuda::NoDevice) = cuda::init() {
-        return;
+    fn dyn_args<H: Handle>(dt_t: DigitLayout, dt_p: DigitLayout) -> Args<H> {
+        use common::dyn_;
+        use std::ptr::{null, null_mut};
+        Args {
+            t_layout: TensorLayout::new(dt_t, &[dyn_(); 3], &[dyn_(); 3]),
+            t_base: null_mut(),
+            p_layout: TensorLayout::new(dt_p, &[dyn_()], &[dyn_()]),
+            p_base: null(),
+            sin_layout: TensorLayout::new(dt_t, &[dyn_(); 2], &[dyn_(); 2]),
+            sin_base: null(),
+            cos_layout: TensorLayout::new(dt_t, &[dyn_(); 2], &[dyn_(); 2]),
+            cos_base: null(),
+            theta: 0.,
+        }
     }
-    let dev = cuda::Device::new(0);
-    println!("{}", dev.info());
 
-    let handle = Gpu::new(dev.context());
-    let mut op = Operator::new(&handle);
+    fn args<H: Handle>(
+        dt_t: DigitLayout,
+        dt_p: DigitLayout,
+        nt: usize,
+        nh: usize,
+        dh: usize,
+        theta: f32,
+        t_base: *mut H::Byte,
+        p_base: *const H::Byte,
+    ) -> Args<H> {
+        use std::ptr::null;
+        Args {
+            t_layout: TensorLayout::new_contiguous(dt_t, [nt, nh, dh]),
+            t_base,
+            p_layout: TensorLayout::new_contiguous(dt_p, [nt]),
+            p_base,
+            sin_layout: TensorLayout::new_contiguous(dt_t, [0, dh]),
+            sin_base: null(),
+            cos_layout: TensorLayout::new_contiguous(dt_t, [0, dh]),
+            cos_base: null(),
+            theta,
+        }
+    }
 
-    op.scheme(&Args {
-        t_layout: TensorLayout::new(F16, &[dyn_(); 3], &[dyn_(); 3]),
-        t_base: null_mut(),
-        p_layout: TensorLayout::new(U32, &[dyn_()], &[dyn_()]),
-        p_base: null(),
-        sin_layout: TensorLayout::new(F32, &[dyn_(); 2], &[dyn_(); 2]),
-        sin_base: null(),
-        cos_layout: TensorLayout::new(F32, &[dyn_(); 2], &[dyn_(); 2]),
-        cos_base: null(),
-        theta: 1e4,
-    })
-    .unwrap();
-    let module = op.scheme.as_ref().unwrap();
-    handle.apply(|ctx| {
-        println!(
-            "{NAME}\n{}",
-            module.load(CString::new(NAME).unwrap(), ctx).info()
-        );
-    })
+    #[test]
+    fn test_compile() {
+        use super::NAME;
+        use std::ffi::CString;
+
+        let Some(gpu) = Gpu::init() else {
+            return;
+        };
+        println!("{}", gpu.0.device().info());
+
+        let mut op = Operator::new(&gpu);
+        op.scheme(&dyn_args(F16, U32)).unwrap();
+
+        let module = op.scheme.as_ref().unwrap();
+        gpu.apply(|ctx| {
+            println!(
+                "{NAME}\n{}",
+                module.load(CString::new(NAME).unwrap(), ctx).info()
+            );
+        })
+    }
+
+    #[test]
+    fn test_compute() {
+        use super::super::common_cpu::Operator as RefOp;
+        use crate::{
+            common_cpu::{Handle as Cpu, ThisThread},
+            nvidia_gpu::cast_load,
+            utils::{Diff, ErrorCollector},
+        };
+        use cuda::memcpy_d2h;
+        use half::f16;
+        use rand::Rng;
+        use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
+
+        let Some(gpu) = Gpu::init() else {
+            return;
+        };
+
+        let mut cpu_op = RefOp::new(&Cpu);
+        let mut gpu_op = Operator::new(&gpu);
+        cpu_op.scheme(&dyn_args(F64, U32)).unwrap();
+        gpu_op.scheme(&dyn_args(F16, U32)).unwrap();
+
+        const NT: usize = 7;
+        let nh = 32;
+        let dh = 64;
+
+        let mut t = vec![0.0f64; NT * nh * dh];
+        rand::thread_rng().fill(&mut t[..]);
+        let p: [u32; NT] = [0, 1, 2, 3, 7, 8, 1];
+
+        let t_ans = gpu.apply(|ctx| {
+            let stream = ctx.stream();
+            let mut t = cast_load(&t, |&x| f16::from_f64(x), &stream);
+            let p = stream.from_host(&p);
+            gpu_op
+                .launch(
+                    &args(
+                        F16,
+                        U32,
+                        NT,
+                        nh,
+                        dh,
+                        1e4,
+                        t.as_mut_ptr().cast(),
+                        p.as_ptr().cast(),
+                    ),
+                    &stream,
+                )
+                .unwrap();
+            let mut host = vec![f16::ZERO; NT * nh * dh];
+            memcpy_d2h(&mut host, &t);
+            host
+        });
+
+        let mut t_ref = t;
+        cpu_op
+            .launch(
+                &args(
+                    F64,
+                    U32,
+                    NT,
+                    nh,
+                    dh,
+                    1e4,
+                    t_ref.as_mut_ptr().cast(),
+                    p.as_ptr().cast(),
+                ),
+                &ThisThread,
+            )
+            .unwrap();
+
+        let diff = t_ref
+            .into_par_iter()
+            .zip(t_ans)
+            .map(|(a, b)| Diff::new(a, b.to_f64()))
+            .collect::<Vec<_>>();
+
+        let mut ec = ErrorCollector::new(f16::EPSILON.to_f64(), 0.);
+        diff.into_iter().for_each(|diff| ec.push(diff));
+        println!("{ec}");
+
+        let (out, count) = ec.summary();
+        assert!(out * 1000 <= count);
+    }
 }
