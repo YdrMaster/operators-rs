8c2227a9626ddbc05c48994e9b92562d5cb7b08e
YdrMaster <ydrml@hotmail.com>
2024-12-04 17:40:30 +0800

    build(infini): 更新 infini-rt
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- operators/build.rs
+++ operators/build.rs
@@ -2,7 +2,7 @@
     use build_script_cfg::Cfg;
     use search_cl_tools::find_opencl;
     use search_cuda_tools::{find_cuda_root, find_nccl_root};
-    use search_infini_tools::find_infer_cc;
+    use search_infini_tools::find_infini_rt;
 
     let cpu = Cfg::new("use_cpu");
     let cl = Cfg::new("use_cl");
@@ -16,7 +16,7 @@
     if cfg!(feature = "opencl") && find_opencl().is_some() {
         cl.define();
     }
-    if cfg!(feature = "infini") && find_infer_cc().is_some() {
+    if cfg!(feature = "infini") && find_infini_rt().is_some() {
         infini.define();
     }
     if cfg!(feature = "nvidia-gpu") && find_cuda_root().is_some() {
