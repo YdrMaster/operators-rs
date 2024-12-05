b9e6fdda526fda69d443e64bdbf53ca9b5f7b480
YdrMaster <ydrml@hotmail.com>
2024-12-05 14:33:37 +0800

    fix(nv): 更新 nccl，支持从 Communicator 创建 NcclNode
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ operators/src/handle/nvidia_gpu/nccl.rs
@@ -0,0 +1,33 @@
+﻿use super::{Config, Gpu};
+use crate::TopoNode;
+use nccl::Communicator;
+use std::sync::Arc;
+
+pub struct NcclNode {
+    gpu: Gpu,
+    pub(crate) nccl: Arc<Communicator>,
+}
+
+impl NcclNode {
+    pub fn new(comm: Communicator, config: Config) -> Self {
+        Self {
+            gpu: Gpu::new(comm.device().retain_primary(), config),
+            nccl: Arc::new(comm),
+        }
+    }
+}
+
+impl TopoNode<Gpu> for NcclNode {
+    #[inline]
+    fn processor(&self) -> &Gpu {
+        &self.gpu
+    }
+    #[inline]
+    fn rank(&self) -> usize {
+        self.nccl.rank()
+    }
+    #[inline]
+    fn group_size(&self) -> usize {
+        self.nccl.count()
+    }
+}
